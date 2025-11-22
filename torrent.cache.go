81fe5a68b4dc3cf530dc560a0abb73670cb8811a
James Lawrence <jljatone@gmail.com>
2025-11-22 18:29:57 -0500

    debugging cache

--- torrent.cache.go
+++ torrent.cache.go
@@ -1,10 +1,10 @@
 package torrent
 
 import (
+	"log"
 	"sync"
 
 	"github.com/james-lawrence/torrent/dht/int160"
-	"github.com/james-lawrence/torrent/internal/langx"
 )
 
 func NewCache(s MetadataStore, b BitmapStore) *memoryseeding {
@@ -24,6 +24,8 @@ type memoryseeding struct {
 }
 
 func (t *memoryseeding) Close() error {
+	log.Println("Close initiated")
+	defer log.Println("Close completed")
 	t._mu.Lock()
 	defer t._mu.Unlock()
 
@@ -38,9 +40,10 @@ func (t *memoryseeding) Close() error {
 
 // sync bitmap to disk
 func (t *memoryseeding) Sync(id int160.T) error {
+	log.Println("Sync initiated", id)
+	defer log.Println("Sync completed", id)
 	t._mu.Lock()
 	defer t._mu.Unlock()
-
 	c, ok := t.torrents[id]
 
 	if !ok {
@@ -58,20 +61,18 @@ func (t *memoryseeding) Sync(id int160.T) error {
 
 // clear torrent from memory
 func (t *memoryseeding) Drop(id int160.T) error {
+	log.Printf("Drop initiated %s\n", id)
+	defer log.Printf("Drop completed %s\n", id)
+	if err := t.Sync(id); err != nil {
+		return err
+	}
+
 	t._mu.Lock()
 	c, ok := t.torrents[id]
 	delete(t.torrents, id)
 	t._mu.Unlock()
-
 	if !ok {
 		return nil
-	}
-
-	// only record if the info is there.
-	if c.haveInfo() {
-		if err := t.bm.Write(id, c.chunks.ReadableBitmap()); err != nil {
-			return err
-		}
 	}
 
 	return c.close()
@@ -87,18 +88,33 @@ func (t *memoryseeding) Insert(md Metadata, fn func(md Metadata, options ...Tune
 		return x, x.Tune(options...)
 	}
 
-	t._mu.Lock()
-	defer t._mu.Unlock()
+	log.Println("Insert initiated", id)
+	defer log.Println("Insert completed", id)
 
-	if x, ok := t.torrents[id]; ok {
-		return x, x.Tune(options...)
+	buildfn := func(id int160.T) (*torrent, error) {
+		t._mu.Lock()
+		defer t._mu.Unlock()
+
+		if x, ok := t.torrents[id]; ok {
+			return x, x.Tune(options...)
+		}
+
+		// only record if the info is there.
+		if len(md.InfoBytes) > 0 {
+			if err := t.MetadataStore.Write(md); err != nil {
+				return nil, err
+			}
+		}
+
+		x := fn(md, options...)
+		t.torrents[id] = x
+
+		return x, nil
 	}
 
-	// only record if the info is there.
-	if len(md.InfoBytes) > 0 {
-		if err := t.MetadataStore.Write(md); err != nil {
-			return nil, err
-		}
+	dlt, err := buildfn(id)
+	if err != nil {
+		return nil, err
 	}
 
 	// if the bitmap cache exists read it to initialize
@@ -106,11 +122,8 @@ func (t *memoryseeding) Insert(md Metadata, fn func(md Metadata, options ...Tune
 	if err != nil {
 		return nil, err
 	}
-
-	dlt := fn(md, tuneVerifySample(unverified, 8), langx.Compose(options...))
-	t.torrents[id] = dlt
-
-	return dlt, nil
+	log.Println("verifying", id)
+	return dlt, dlt.Tune(tuneVerifySample(unverified, 8))
 }
 
 func (t *memoryseeding) Load(id int160.T, fn func(md Metadata, options ...Tuner) *torrent, options ...Tuner) (dlt *torrent, cached bool, _ error) {
@@ -121,6 +134,9 @@ func (t *memoryseeding) Load(id int160.T, fn func(md Metadata, options ...Tuner)
 	if ok {
 		return x, true, x.Tune(options...)
 	}
+
+	log.Println("Load initiated", id)
+	defer log.Println("Load completed", id)
 
 	buildfn := func(id int160.T) (*torrent, bool, error) {
 		t._mu.Lock()
@@ -154,12 +170,15 @@ func (t *memoryseeding) Load(id int160.T, fn func(md Metadata, options ...Tuner)
 		return nil, false, err
 	}
 
+	log.Println("verifying", id)
 	return dlt, cached, dlt.Tune(tuneVerifySample(unverified, 8))
 }
 
 func (t *memoryseeding) Metadata(id int160.T) (md Metadata, err error) {
-	t._mu.Lock()
-	defer t._mu.Unlock()
+	log.Println("Metadata initiated", id)
+	defer log.Println("Metadata completed", id)
+	t._mu.RLock()
+	defer t._mu.RUnlock()
 
 	if x, ok := t.torrents[id]; ok {
 		return x.md, nil
