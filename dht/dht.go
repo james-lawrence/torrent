0f5f3d97cb2b5fdf97df136235a88d85e3f42bf9
James Lawrence <jljatone@gmail.com>
2026-03-05 07:59:28 -0500

    fix static port function

--- dht/dht.go
+++ dht/dht.go
@@ -181,11 +181,9 @@ var DefaultGlobalBootstrapHostPorts = []string{
 	"router.utorrent.com:6881",
 	"router.bittorrent.com:6881",
 	"dht.transmissionbt.com:6881",
-	"dht.aelitis.com:6881",     // Vuze
-	"router.silotis.us:6881",   // IPv6
-	"dht.libtorrent.org:25401", // @arvidn's
-	"dht.anacrolix.link:42069",
-	"router.bittorrent.cloud:42069",
+	"dht.aelitis.com:6881",
+	"router.silotis.us:6881",
+	"dht.libtorrent.org:25401",
 }
 
 // Returns the resolved addresses of the default global bootstrap nodes. Network is unused but was
--- dht/static.port.go
+++ dht/static.port.go
@@ -10,26 +10,60 @@ import (
 
 	"github.com/james-lawrence/torrent/dht/int160"
 	"github.com/james-lawrence/torrent/dht/traversal2"
+	"github.com/james-lawrence/torrent/internal/errorsx"
 )
 
 func AutoDetectIP(ctx context.Context, q *Server, id int160.T, local net.PacketConn, port uint16) (iter.Seq[netip.AddrPort], error) {
 	return func(yield func(netip.AddrPort) bool) {
-		const lease = time.Hour
+		const (
+			minK       = 8
+			shortretry = time.Minute
+			lease      = time.Hour
+		)
+
+		collect := func() (map[netip.AddrPort]struct{}, error) {
+			_ctx, done := context.WithTimeout(ctx, time.Minute)
+			defer done()
+
+			detect := traversal2.New(int160.Random(), NewTraversalQuerier(q), traversal2.WithSeeds(q.ClosestGoodNodeInfos(minK, id)...))
+			m := map[netip.AddrPort]struct{}{}
+			for v := range detect.Results(_ctx) {
+				addr := netip.AddrPortFrom(v.IP.Addr(), port)
+				if !addr.IsValid() {
+					continue
+				}
+
+				m[addr] = struct{}{}
+			}
+
+			return m, errorsx.Ignore(detect.Err(), context.DeadlineExceeded)
+		}
 
 		for {
-			detect := traversal2.New(int160.Random(), NewTraversalQuerier(q), traversal2.WithSeeds(q.ClosestGoodNodeInfos(8, id)...))
-
-			for v := range detect.Results(ctx) {
-				log.Println("DERP DERP DERP", netip.AddrPortFrom(v.IP.Addr(), port))
-				if !yield(netip.AddrPortFrom(v.IP.Addr(), port)) {
+			if gnodes := q.numGoodNodes(); gnodes < minK {
+				if !yield(*q.dynamicaddr.Load()) {
 					return
 				}
 
-				break
+				time.Sleep(200 * time.Millisecond)
+				continue
 			}
 
-			if err := detect.Err(); err != nil {
+			m, err := collect()
+
+			for addr := range m {
+				if !yield(addr) {
+					return
+				}
+			}
+
+			if err != nil {
 				log.Println("auto detect port failed", err)
+			}
+
+			if len(m) == 0 {
+				time.Sleep(shortretry)
+				continue
 			}
 
 			time.Sleep(lease)
