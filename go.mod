7f3da4187bfcb21e3c0cb0c0eda1ffc27f78c55e
James Lawrence <jljatone@gmail.com>
2026-05-03 08:51:58 -0400

    have sockets auto detect their best address
    when bound to the unspecified address have the sockets cache their best
    netip.Addr.

--- go.mod
+++ go.mod
@@ -1,14 +1,13 @@
 module github.com/james-lawrence/torrent
 
-go 1.24.0
-
-toolchain go1.24.6
+go 1.25.0
 
 require (
-	filippo.io/edwards25519 v1.1.0
-	github.com/RoaringBitmap/roaring/v2 v2.14.4
+	filippo.io/edwards25519 v1.2.0
+	github.com/RoaringBitmap/roaring/v2 v2.18.0
 	github.com/alexflint/go-arg v1.6.0
-	github.com/anacrolix/go-libutp v1.3.2
+	github.com/anacrolix/generics v0.2.0
+	github.com/anacrolix/go-libutp v1.4.0
 	github.com/anacrolix/missinggo v1.3.0
 	github.com/anacrolix/multiless v0.4.0
 	github.com/anacrolix/stm v0.5.0
@@ -18,24 +17,23 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
 	github.com/edsrzf/mmap-go v1.2.0
 	github.com/frankban/quicktest v1.14.6
-	github.com/fsnotify/fsnotify v1.9.0
+	github.com/gofrs/uuid/v5 v5.4.0
 	github.com/google/btree v1.1.3
 	github.com/google/go-cmp v0.7.0
-	github.com/rs/dnscache v0.0.0-20230804202142-fc85eb664529
 	github.com/stretchr/testify v1.11.1
 	go.opencensus.io v0.24.0
-	golang.org/x/exp v0.0.0-20251113190631-e25ba8c21ef6
-	golang.org/x/net v0.47.0
-	golang.org/x/time v0.14.0
+	golang.org/x/exp v0.0.0-20260410095643-746e56fc9e2f
+	golang.org/x/net v0.53.0
+	golang.org/x/time v0.15.0
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da
-	github.com/anacrolix/generics v0.1.0
 )
 
 require (
+	github.com/alecthomas/repr v0.5.2 // indirect
 	github.com/anacrolix/log v0.17.0 // indirect
 	github.com/anacrolix/missinggo/perf v1.0.0 // indirect
 	github.com/anacrolix/mmsg v1.0.1 // indirect
-	github.com/anacrolix/sync v0.4.0 // indirect
+	github.com/anacrolix/sync v0.5.4 // indirect
 )
 
 require (
@@ -43,7 +41,7 @@ require (
 	github.com/alecthomas/atomic v0.1.0-alpha2 // indirect
 	github.com/alexflint/go-scalar v1.2.0 // indirect
 	github.com/anacrolix/missinggo/v2 v2.10.0 // indirect
-	github.com/bits-and-blooms/bitset v1.24.3 // indirect
+	github.com/bits-and-blooms/bitset v1.24.4 // indirect
 	github.com/bradfitz/iter v0.0.0-20191230175014-e8f45d346db8 // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -51,8 +49,7 @@ require (
 	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	golang.org/x/sync v0.18.0 // indirect
-	golang.org/x/sys v0.38.0
+	golang.org/x/sys v0.43.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- sockets/sockets.go
+++ sockets/sockets.go
@@ -1,19 +1,82 @@
 package sockets
 
 import (
+	"cmp"
 	"context"
+	"log"
 	"net"
+	"net/netip"
+	"slices"
+	"sync/atomic"
+	"time"
 
+	"github.com/james-lawrence/torrent/internal/errorsx"
+	"github.com/james-lawrence/torrent/internal/langx"
 	"github.com/james-lawrence/torrent/internal/netx"
+	"github.com/james-lawrence/torrent/internal/slicesx"
 )
+
+const addrCacheTTL = 5 * time.Minute
+
+type cachedAddr struct {
+	addr    netip.AddrPort
+	netaddr net.Addr
+	ts      time.Time
+}
+
+func cmpaddrport(a, b netip.AddrPort) int {
+	return cmp.Compare(netx.AddrPortPriority(a), netx.AddrPortPriority(b))
+}
+
+// computeBestAddr returns the best routable local address for a bound listener.
+// When the listener is bound to an unspecified address (0.0.0.0 / ::),
+// it enumerates interface addresses and picks the highest-priority one.
+func computeBestAddr(bound net.Addr) netip.AddrPort {
+	ap := errorsx.Zero(netx.AddrPort(bound))
+	defer log.Println("DERP DERP", ap)
+	if !ap.Addr().Unmap().IsUnspecified() {
+		return netip.AddrPortFrom(ap.Addr().Unmap(), ap.Port())
+	}
+
+	ifaces, err := net.InterfaceAddrs()
+	if err != nil {
+		return ap
+	}
+
+	ips := slicesx.MapTransform(func(n net.Addr) netip.AddrPort {
+		switch v := n.(type) {
+		case *net.IPNet:
+			addr, _ := netip.AddrFromSlice(v.IP)
+			return netip.AddrPortFrom(addr.Unmap(), ap.Port())
+		case *net.IPAddr:
+			addr, _ := netip.AddrFromSlice(v.IP)
+			return netip.AddrPortFrom(addr.Unmap(), ap.Port())
+		default:
+			return netip.AddrPortFrom(netip.Addr{}, ap.Port())
+		}
+	}, ifaces...)
+	slices.SortStableFunc(ips, cmpaddrport)
+	return langx.FirstNonZero(ips...)
+}
+
+func addrOf(ap netip.AddrPort, network string) net.Addr {
+	ip := net.IP(ap.Addr().AsSlice())
+	port := int(ap.Port())
+	switch network {
+	case "udp", "udp4", "udp6":
+		return &net.UDPAddr{IP: ip, Port: port}
+	default:
+		return &net.TCPAddr{IP: ip, Port: port}
+	}
+}
 
 // New creates a socket from a net listener and dialer.
 func New(l net.Listener, d netx.Dialer) Socket {
 	if l, ok := l.(packetlistener); ok {
-		return packetconnSocket{packetlistener: l, dialer: d}
+		return &packetconnSocket{packetlistener: l, dialer: d}
 	}
 
-	return socket{Listener: l, dialer: d}
+	return &socket{Listener: l, dialer: d}
 }
 
 // Socket for torrent clients
@@ -33,19 +96,45 @@ type packetlistener interface {
 type packetconnSocket struct {
 	packetlistener
 	dialer netx.Dialer
+	cached atomic.Pointer[cachedAddr]
+}
+
+// Addr returns the best available local address for this socket.
+func (t *packetconnSocket) Addr() net.Addr {
+	if c := t.cached.Load(); c != nil && time.Since(c.ts) <= addrCacheTTL {
+		return c.netaddr
+	}
+	bound := t.packetlistener.Addr()
+	ap := computeBestAddr(bound)
+	na := addrOf(ap, bound.Network())
+	t.cached.Store(&cachedAddr{addr: ap, netaddr: na, ts: time.Now()})
+	return na
 }
 
 // Dial remote peers from this socket.
-func (t packetconnSocket) Dial(ctx context.Context, addr string) (conn net.Conn, err error) {
+func (t *packetconnSocket) Dial(ctx context.Context, addr string) (conn net.Conn, err error) {
 	return t.dialer.DialContext(ctx, t.packetlistener.Addr().Network(), addr)
 }
 
 type socket struct {
 	net.Listener
 	dialer netx.Dialer
+	cached atomic.Pointer[cachedAddr]
+}
+
+// Addr returns the best available local address for this socket.
+func (t *socket) Addr() net.Addr {
+	if c := t.cached.Load(); c != nil && time.Since(c.ts) <= addrCacheTTL {
+		return c.netaddr
+	}
+	bound := t.Listener.Addr()
+	ap := computeBestAddr(bound)
+	na := addrOf(ap, bound.Network())
+	t.cached.Store(&cachedAddr{addr: ap, netaddr: na, ts: time.Now()})
+	return na
 }
 
 // Dial remote peers from this socket.
-func (t socket) Dial(ctx context.Context, addr string) (conn net.Conn, err error) {
+func (t *socket) Dial(ctx context.Context, addr string) (conn net.Conn, err error) {
 	return t.dialer.DialContext(ctx, t.Listener.Addr().Network(), addr)
 }
