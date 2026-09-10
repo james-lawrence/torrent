fca70f1fb0210fab622a0df0466258ef0ebb99aa
James Lawrence <jljatone@gmail.com>
2026-09-10 16:02:35 -0400

    adjust endgame to 15%

--- chunks.go
+++ chunks.go
@@ -810,7 +810,7 @@ func copRequestPool(c *chunks) *roaring.Bitmap {
 		return c.missing
 	}
 
-	if float64(c.pieces-c.completed.GetCardinality())/float64(c.pieces) > 0.05 {
+	if float64(c.pieces-c.completed.GetCardinality())/float64(c.pieces) > 0.15 {
 		return c.missing
 	}
 
